7bc2a4cdd4bb80aa69cead6864857faffcded163
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2022-05-21 13:38:35 -0300

    Implements the base project

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// https://blog.groupdocs.com/2020/12/29/remove-metadata-of-documents-and-images-using-csharp/
